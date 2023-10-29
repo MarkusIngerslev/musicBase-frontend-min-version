a2b5ab7f5bb099356c0db4ed5bc3f0f21a0e9542
MarkusIngerslev <markusolsen2004@gmail.com>
2023-10-29 16:22:31 +0100

    Minor changes for console.log

--- js/restService.js
+++ js/restService.js
@@ -1,33 +1,32 @@
 "use strict";
 
 // Endpoint
-const port = 3333;
-const endpoint = `http://localhost:${port}`;
+const endpoint = `http://localhost:3333`;
 
 // GET JSON-data for Albums
 async function getAlbumData() {
     const res = await fetch(`${endpoint}/albums`);
-    console.log(res);
+    // console.log(res);
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
     return data;
 }
 
 // GET JSON-data for Tracks
 async function getTrackData() {
     const res = await fetch(`${endpoint}/tracks`);
-    console.log(res);
+    // console.log(res);
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
     return data;
 }
 
 // GET JSON-data for Artists
 async function getArtistData() {
     const res = await fetch(`${endpoint}/artists`);
-    console.log(res);
+    // console.log(res);
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
     return data;
 }
 
