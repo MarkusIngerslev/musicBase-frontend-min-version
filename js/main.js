4044213c14d6c69f6178791377ef993f5b3894df
MarkusIngerslev <markusolsen2004@gmail.com>
2023-10-29 16:50:57 +0100

    Added functionality to TrackRenderer so tracks now show in "sange" tabs on site

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ import { getAlbumData, getTrackData, getArtistData } from "./restService.js";
 import { ListRenderer } from "./renderers/ListRenderer.js";
 import { AlbumRenderer } from "./renderers/AlbumRenderer.js";
 import { ArtistRenderer } from "./renderers/ArtistRenderer.js";
+import { TrackRenderer } from "./renderers/TrackRenderer.js";
 
 // ===== Global varibles ===== \\
 // array for data from database
@@ -33,15 +34,42 @@ async function initApp() {
     artists = await getArtistData();
     tracks = await getTrackData();
 
+    // Group tracks by trackID
+    const groupedTracks = groupTracksByTrackID(tracks);
+
     // Create instance of Renderes
     const albumRenderer = new AlbumRenderer();
     const artistRenderer = new ArtistRenderer();
+    const trackRenderer = new TrackRenderer(albums);
 
     // Display lists
     const albumList = new ListRenderer(albums, "#albums-table-tbody", albumRenderer);
     const artistList = new ListRenderer(artists, "#artists-table-tbody", artistRenderer);
-    const trackList = new ListRenderer(tracks, "#tracks-table-tbody", null);
+    const trackList = new ListRenderer(groupedTracks, "#tracks-table-tbody", trackRenderer);
 
     albumList.render();
     artistList.render();
+    trackList.render();
+}
+
+// ===== FUNKTIONS ===== \\
+// function to group tracks by their ID
+function groupTracksByTrackID(tracks) {
+    const groupedTracks = {};
+
+    for (const track of tracks) {
+        if (groupedTracks.hasOwnProperty(track.trackID)) {
+            // If the trackID is already in the groupedTracks object, add the albumID to the existing entry
+            groupedTracks[track.trackID].albumIDs.push(track.albumID);
+        } else {
+            // If the trackID is not in the groupedTracks object, create a new entry
+            groupedTracks[track.trackID] = {
+                ...track,
+                albumIDs: [track.albumID],
+            };
+        }
+    }
+
+    // Convert the groupedTracks object back to an array
+    return Object.values(groupedTracks);
 }
---
+++ js/renderers/TrackRenderer.js
@@ -0,0 +1,32 @@
+export class TrackRenderer {
+    constructor(albums) {
+        this.albums = albums;
+    }
+
+    render(track) {
+        if (!track) {
+            console.error("Track is undefined or null");
+            return "";
+        }
+        // Find all albums associated with the track's albumIDs
+        const albumTitles = track.albumIDs.map((albumID) => {
+            const album = this.albums.find((album) => album.albumID === albumID);
+            return album ? album.albumTitle : "Unknown Album";
+        });
+
+        // Combine album titles with commas
+        const albumTitlesString = albumTitles.join(", ");
+
+        // Create html for tbody
+        let html = /*html */ `
+            <tr class='track-row' data-track-id='${track.trackID}'>
+                <td>${track.trackName}</td>
+                <td>${track.duration}</td>
+                <td>${track.artistNames.join(", ")}</td>
+                <td>${albumTitlesString}</td> <!-- Display the album titles -->
+            </tr>
+        `;
+
+        return html;
+    }
+}
